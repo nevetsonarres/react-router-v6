bd2bcf9b4e6048971afe7b609fb01ff83633e2d4
nevetsonarres <stevendserrano@gmail.com>
2023-02-05 23:36:34 +0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { 
+  BrowserRouter, 
+  Link, 
+  Navigate, 
+  Route,
+  Routes
+  // Switch 
+} from 'react-router-dom'
 
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
+import { useState } from 'react'
 
 function App() {
+
+  const [cartIsEmpty] = useState(false);
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -16,18 +27,29 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/products">Products</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
+        <Routes>
+          {/* <Route path="/" component={Home} /> */}
+          <Route path="/" element={<Home />} />
+
+          <Route path="/about/*" element={<About />}/>
+
+          <Route path="/products/:id/*" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          {/* In line JSX */}
+          <Route path="/test" element={
+            <div>
+              <h2>Test Page</h2>
+              <p>hello!</p>
+            </div>
+          } />
+          {/* v5 <Route path="/redirect" element={<Redirect to="/about" />} /> */}
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+          <Route 
+            path="/checkout"
+            // ternary expression
+            element={ cartIsEmpty ? <Navigate to="/products" /> : <p>Checkout</p> }
+          />
+        </Routes> 
       </BrowserRouter>
     </div>
   )
